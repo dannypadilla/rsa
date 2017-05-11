85f47e95a1e6b5abce049ffee6313947c118ecc5
Ken Luo <luozhirong@gmail.com>
2017-05-11 13:22:24 -0700

    started MD5

--- src/rsa/DigitalSignature.java
+++ src/rsa/DigitalSignature.java
@@ -1,6 +1,34 @@
 package src.rsa;
 
-public class DigitalSignature {
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.security.*;
+
+public class DigitalSignature{
+
+    private Byte [] byteArray;
+
+
+
+    DigitalSignature(){
+        try {
+            MessageDigest MD = MessageDigest.getInstance("MD5");
+            try {
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream("test.txt"));
+                
+            }
+            catch(Exception ex){
+                ex.printStackTrace();
+            }
+
+        }
+        catch(NoSuchAlgorithmException x){
+            System.err.println("Broken");
+        }
+    }
+
+
 
 
 
